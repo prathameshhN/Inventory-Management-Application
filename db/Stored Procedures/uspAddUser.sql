04d2eead6673d0d871338d5adb0ce58eb25e47f7
Prathamesh_N <99020039+prathameshhN@users.noreply.github.com>
2024-12-09 15:45:51 +0530

    Update uspAddUser.sql

--- db/Stored Procedures/uspAddUser.sql
+++ db/Stored Procedures/uspAddUser.sql
@@ -1,5 +1,5 @@
 GO
-/****** Object:  StoredProcedure [dbo].[uspAddUser]    Script Date: 2/15/2020 10:09:15 AM ******/
+/****** Object:  StoredProcedure [dbo].[uspAddUser]    
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
